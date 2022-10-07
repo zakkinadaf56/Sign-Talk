6c5aa704f7742fec230b1a63f3c7778e7dec85f4
Zakki <zakkinadaf56@gmail.com>
2022-10-07 15:40:37 +0530

    updated test file

--- test.py
+++ test.py
@@ -9,17 +9,18 @@ import tensorflow
 
 cam=cv.VideoCapture(0)
 detector=HandDetector(maxHands=1)
-classifier=Classifier("Model/keras_model.h5","Model/label.txt")
+classifier=Classifier("Model/keras_model.h5","Model/labels.txt")
 
 offset=20
 imgSize=300
 
 folder="data/C"
 counter=0
-labels=["A","B","C"]
+labels=["A","B","C","D","E","F","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","G","H","1","3","4","5","7","8","10"]
 
 while True:
     success,img=cam.read()
+    imgOutput=img.copy()
     hands,img=detector.findHands(img)
     if hands:
         hand=hands[0]
@@ -38,7 +39,7 @@ while True:
             imgResizeShape=imgResize.shape
             wGap=math.ceil((imgSize-wCal)/2)
             imgWhite[0:imgResizeShape[0],wGap:wCal+wGap]=imgResize
-            prediction,index=classifier.getPrediction(img)
+            prediction,index=classifier.getPrediction(imgWhite,draw=False)
             print(prediction,index)
 
         else:
@@ -48,13 +49,18 @@ while True:
             imgResizeShape=imgResize.shape
             hGap=math.ceil((imgSize-hCal)/2)
             imgWhite[hGap:hCal+hGap,0:imgResizeShape[1]]=imgResize
+            prediction,index=classifier.getPrediction(imgWhite,draw=False)
 
+
+        cv.rectangle(imgOutput,(x-offset,y-offset-50),(x-offset+90,y-offset-50+50),(255,0,255),cv.FILLED)
+        cv.putText(imgOutput,labels[index],(x,y-25),cv.FONT_HERSHEY_COMPLEX,1.7,(255,255,255),2)
+        cv.rectangle(imgOutput,(x-offset,y-offset),(x+w+offset,y+h+offset),(255,0,255),4)
 
         cv.imshow("cropped",imgCrop)
         cv.imshow("White",imgWhite)
 
 
-    cv.imshow("Image",img)
+    cv.imshow("Image",imgOutput)
     cv.waitKey(1)
      
 
